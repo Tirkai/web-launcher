777700196fc6eb76f5881dbcf1076ca476439630
Дмитрий Буренков <dmitrii.burenkov@fortis.online>
2023-09-22 23:12:29 +0300

    Test

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,15 @@
-import { createSignal } from "solid-js";
+import { createResource, createSignal } from "solid-js";
 import "./Counter.css";
 
+const fetcher = async () => {
+  const response = await fetch("http://localhost:8080/api");
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
 export default function Counter() {
-  const [count, setCount] = createSignal(0);
-  return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
-      Clicks: {count()}
-    </button>
-  );
+  const [data] = createResource(fetcher);
+  return <div>Data: {JSON.stringify(data())}</div>;
 }
